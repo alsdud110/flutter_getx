562f92e268856e064edae62867c383da28a1ca58
황민영 <alsdud110@jness.co.kr>
2023-11-16 13:36:36 +0900

    설명 주석 추가

--- lib/domain/user/user_repository.dart
+++ lib/domain/user/user_repository.dart
@@ -11,10 +11,47 @@ class UserRepository {
 
   Future<User> login(String username, String password) async {
     LoginReqDto dto = LoginReqDto(username, password);
+    /*
+      dto.toJson() 을 하면 객체를 Map타입으로 변경
+
+      Map<String, dynamic> toJson() => {
+        "username": username,
+        "password": password,
+      };
+    
+     */
 
     Response response = await _userProvider.login(dto.toJson());
     dynamic headers = response.headers;
     dynamic body = response.body;
+
+    /*
+      dynamic body = response.body 를 받으면 어떻게 되냐
+      {
+        code: 1,
+        token: ~,
+        msg: 성공/실패,
+        data : {
+          id: 1,
+          username: ~,
+          password: ,
+          email: ,
+          created: ,
+          updated: ,
+        }
+      }
+    
+      위처럼 저장됨 body에
+      그럼 Dart 객체로 변환시켜줘야함
+      CMRespDto cmRespDto = CMRespDto.fromJson(body) 를 하면 Map타입을 Dart객체로 변경
+
+      CMRespDto.fromJson(Map<String, dynamic> json)
+      : code = json["code"],
+        msg = json["msg"],
+        data = json["data"];
+
+      그럼 cmRespDto.code, msg, data 이런식으로 사용 가능
+     */
 
     CMRespDto cmRespDto = CMRespDto.fromJson(body);
     if (cmRespDto.code == 1) {
